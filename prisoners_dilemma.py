a29e8654a339e415b5930c8a082e234e86923854
Dburke3158 <dburke-student@nfcs.k12.pa.us>
2016-02-02 11:08:07 -0500

    Changed Date
    I changed the Date.

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -1,5 +1,8 @@
 from __future__ import print_function
 
+#Name: Devon Burke Alaysha Tavarez
+#Date: 2/2/16
+
 ''' 
 PrisonerDilemma.py allows hard-coding different strategies
 for the Iterative Prisoners Dilemma, the canonical game of game-theory.
